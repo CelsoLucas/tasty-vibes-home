325deeec3b3f12d905d784d8343f111742a2c3a3
Celso Lucas <lucascelso23@gmail.com>
2025-08-21 21:21:52 -0400

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css';
-import 'react-native-url-polyfill/auto';
+import 'react-native-url-polyfill/auto'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
